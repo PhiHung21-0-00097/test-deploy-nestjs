a9fe6c33651750fac41f58c05be67d7a57204514
Phihung21-0-00097 <0943244904hung@gmail.com>
2024-09-18 18:13:50 +0700

    up

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3333);
 }
